f2574747d1ea1342fc132b184c4637afe9fadf01
Pascal Precht <pascal.precht@gmail.com>
2019-10-09 21:55:25 +0900

    WIP SAVEPOINT

---
+++ contracts/simple_storage_nim.nim
@@ -0,0 +1,12 @@
+import nimplay0_1
+
+contract("SimpleStorageNim"):
+
+  var
+    storedData*: uint256 = 100
+  proc set*(x: uint256) {.self.} =
+    self.storedData = x
+  proc get*(): uint256 {.self.} =
+    self.storedData
+
+
